9380ab1e82cb7cd14cd9e6d4fb6bf00fed0e2985
Michał Witas <mwitas@future-processing.com>
2021-08-06 17:58:59 +0200

    Passwords as Keys

--- t007_hashaspassword.py
+++ t007_hashaspassword.py
@@ -1,23 +1,3 @@
-'''curl 'http://aes.cryptohack.org/passwords_as_keys/decrypt/c92b7734070205bdf6c0087a751466ec13ae15e6f1bcdd3f3a535ec0f4bbae66/aa/' \
-  -H 'Connection: keep-alive' \
-  -H 'Accept: */*' \
-  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' \
-  -H 'X-Requested-With: XMLHttpRequest' \
-  -H 'Sec-GPC: 1' \
-  -H 'Referer: http://aes.cryptohack.org/passwords_as_keys/' \
-  -H 'Accept-Language: en-US,en;q=0.9,pl;q=0.8' \
-  --compressed \
-  --insecure ;
-curl 'http://aes.cryptohack.org/passwords_as_keys/decrypt/c92b7734070205bdf6c0087a751466ec13ae15e6f1bcdd3f3a535ec0f4bbae66/aabb/' \
-  -H 'Connection: keep-alive' \
-  -H 'Accept: */*' \
-  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' \
-  -H 'X-Requested-With: XMLHttpRequest' \
-  -H 'Sec-GPC: 1' \
-  -H 'Referer: http://aes.cryptohack.org/passwords_as_keys/' \
-  -H 'Accept-Language: en-US,en;q=0.9,pl;q=0.8' \
-  --compressed \
-  --insecure'''
 import hashlib
 import os
 from Crypto.Cipher import AES
